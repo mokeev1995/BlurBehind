285795d002f2d50749f4da795279ab18b7e4c3e7
Mokeev Andrey <andrey@mokeev1995.ru>
2015-11-03 01:14:51 +0300

    visual changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Interop;
 
 namespace BlurBehindDemo
@@ -49,17 +50,37 @@ namespace BlurBehindDemo
 		public MainWindow()
 		{
 			InitializeComponent();
+			MouseLeftButtonDown += delegate {
+				try
+				{
+					DragMove();
+				}
+				catch (Exception)
+				{
+					// ignored
+				}
+			};
+			Block.MouseLeftButtonDown += delegate {
+				try
+				{
+					DragMove();
+				}
+				catch (Exception)
+				{
+					// ignored
+				}
+			};
 		}
-		
+
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
 			EnableBlur();
 		}
-		
+
 		internal void EnableBlur()
 		{
 			var windowHelper = new WindowInteropHelper(this);
-			
+
 			var accent = new AccentPolicy();
 			accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
 
@@ -72,7 +93,7 @@ namespace BlurBehindDemo
 			data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
 			data.SizeOfData = accentStructSize;
 			data.Data = accentPtr;
-			
+
 			SetWindowCompositionAttribute(windowHelper.Handle, ref data);
 
 			Marshal.FreeHGlobal(accentPtr);
