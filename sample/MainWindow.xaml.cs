0939ea52e55aeb94c8486a41d9529af0deeb655a
Mokeev Andrey <andrey@mokeev1995.ru>
2015-12-13 17:04:02 +0300

    removed old code

--- sample/MainWindow.xaml.cs
+++ sample/MainWindow.xaml.cs
@@ -36,17 +36,6 @@ namespace BlurBehindDemo
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
 			BlurWindow.SetBlurWindow(this);
-			var regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
-			var product = regKey?.GetValue("ProductName");
-			Text.Content = $"Windows Version: v{Environment.OSVersion.Version.Major}.{Environment.OSVersion.Version.Minor}, PRODUCT: {product}";
-		}
-
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-			BlurWindow.SetBlurWindow(this);
-			var regKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
-			var product = regKey?.GetValue("ProductName");
-			Text.Content = $"Windows Version: v{Environment.OSVersion.Version.Major}.{Environment.OSVersion.Version.Minor}, PRODUCT: {product}";
 		}
 	}
 
